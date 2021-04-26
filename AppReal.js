01e36b3f937cad79be94387a43f28190042f68d8
Alisiyad9 <alisiyad9@gmail.com>
2021-04-26 18:24:24 +0100

    26

--- AppReal.js
+++ AppReal.js
@@ -1,3 +1,16 @@
 function add(a, b) {
   return a * b;
 }
+
+// console.log(' how are you doing hibo');
+
+let hibo = {
+  address: '80 prentice court',
+  greeting: 'Hello hibo',
+};
+
+console.log(hibo.address + ' ' + hibo.greeting);
+
+hibo.age = 2666;
+
+console.log(hibo.age);
